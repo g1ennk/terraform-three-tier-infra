ee258fd15d4f5561fc536eb72ed22aed59eedcf7
glenn <mskim.glenn@gmail.com>
2025-03-21 04:52:36 +0000

    feat: add RDS module

---
+++ modules/rds/main.tf
@@ -0,0 +1,23 @@
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group"
+  subnet_ids = var.subnet_ids
+  tags       = merge(var.common_tags, { Name = "rds-subnet-group" })
+}
+
+resource "aws_db_instance" "rds" {
+  identifier           = var.db_identifier
+  allocated_storage    = var.allocated_storage
+  engine               = var.engine
+  engine_version       = var.engine_version
+  instance_class       = var.instance_class
+  db_name              = var.db_name
+  username             = var.db_username
+  password             = var.db_password
+  port                 = var.db_port
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
+  multi_az             = var.multi_az
+  publicly_accessible  = false
+  skip_final_snapshot  = true
+
+  tags = merge(var.common_tags, { Name = "rds-instance" })
+}
