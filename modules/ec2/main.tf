2e902414d748a3571fdbfdd28f315dac52a8280a
glenn <mskim.glenn@gmail.com>
2025-03-28 05:27:13 +0000

    [EC2 모듈] Launch Template에 IAM Instance Profile 추가 및 CodeDeploy용 IAM Role 구성

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,6 +4,9 @@ resource "aws_launch_template" "ec2_lt" {
   image_id      = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
+  iam_instance_profile {
+    name = aws_iam_instance_profile.codedeploy_ec2_profile.name
+  }
 
   network_interfaces {
     associate_public_ip_address = false
@@ -31,7 +34,51 @@ resource "aws_autoscaling_group" "ec2_asg" {
 
   tag {
     key                 = "Name"
-    value               = "ec2-asg"
+    value               = var.ec2_tag_name
     propagate_at_launch = true
   }
+}
+
+# IAM
+resource "aws_iam_role" "codedeploy_ec2_role" {
+  name = "CodeDeploy-EC2-Role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = var.common_tags
+}
+
+# 권한 1: CodeDeploy
+resource "aws_iam_role_policy_attachment" "codedeploy_agent_policy" {
+  role       = aws_iam_role.codedeploy_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy"
+}
+
+# 권한 2: ECR Pull
+resource "aws_iam_role_policy_attachment" "ecr_read_policy" {
+  role       = aws_iam_role.codedeploy_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+# 권한 3: S3 Read
+resource "aws_iam_role_policy_attachment" "s3_read_policy" {
+  role       = aws_iam_role.codedeploy_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+# EC2 인스턴스 프로파일
+resource "aws_iam_instance_profile" "codedeploy_ec2_profile" {
+  name = "CodeDeploy-EC2-InstanceProfile"
+  role = aws_iam_role.codedeploy_ec2_role.name
 }
